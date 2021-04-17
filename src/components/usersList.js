4224d53430d434399ef0b136303aec1112c9965f
George Emad <george.emad@outlook.com>
2021-04-17 15:22:29 +0200

    NEW: users count workig

--- src/components/usersList.js
+++ src/components/usersList.js
@@ -6,28 +6,57 @@ import axios from 'axios'
 function Users() {
 
     const [users,setUsers] = useState()
+    const [usersWithCount, setUsersWithCount] = useState()
     const [interests,setInterests] = useState()
+    const [aaa, setAaa] = useState(false)
 
     useEffect(() => {
         // get users
         axios.get('users.json')
-        .then(res => setUsers(res.data))
+        .then(res => {
+            setUsers(res.data)
+            setAaa(true)
+        })
         .catch(err => console.log(err))
 
         // get interests
         axios.get('interests.json')
-        .then(res => setInterests(res.data))
+        .then(res => setInterests(res.data)) 
         .catch(err => console.log(err))
         
     }, [])
 
+    let followersCount = users?.map(user => {
+        const followersLength = users?.filter(c => c.id !== user.id && c.following.indexOf(user.id) > -1)?.length;
+        return { id: user.id, followers: followersLength };});
 
- let followersCount = users?.map(user => {
-    const followersLength = users?.filter(c => c.id !== user.id && c.following.indexOf(user.id) > -1)?.length;
-    return { id: user.id, followers: followersLength };});
+    useEffect(() => {
+        if(users) {
+            console.log('0000', users)
+            let newwww = users
+            console.log('newwww', newwww)
+            // setUsers([...newwww, {count: 'hello'}])
+            // setUsers(newwww.forEach((element)=> element.count = 'hello'))
+            setUsers(newwww.forEach((element)=> element.count = 
+            newwww?.filter(c => c.id !== element.id && c.following.indexOf(element.id) > -1)?.length
+            ))
 
-console.log('++++', 
-followersCount
+
+        }
+    }, [aaa])
+
+    
+
+//  let followersCount = users?.map(user => {
+//     const followersLength = users?.filter(c => c.id !== user.id && c.following.indexOf(user.id) > -1)?.length;
+//     return { id: user.id, followers: followersLength };});
+
+// console.log('++++', 
+// followersCount
+// )
+
+console.log('users', 
+users
 )
 
     return (
