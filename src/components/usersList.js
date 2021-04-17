d4fabd10f3e388889a5b74aec796de4b63f0a2b2
George Emad <george.emad@outlook.com>
2021-04-17 11:56:50 +0200

    NEW: 1. install axios 2. add json files ( mock data ) at public folder to use axios 3. get requests for users & interests

--- src/components/usersList.js
+++ src/components/usersList.js
@@ -1,8 +1,28 @@
 import React, {useState,useEffect} from 'react'
+import axios from 'axios'
 
-import {interestsMock,usersMock} from '../API'
+
 
 function Users() {
+
+    const [users,setUsers] = useState()
+    const [interests,setInterests] = useState()
+
+
+    useEffect(() => {
+        // get users
+        axios.get('users.json')
+        .then(res => setUsers(res.data))
+        .catch(err => console.log(err))
+
+        // get interests
+        axios.get('interests.json')
+        .then(res => setInterests(res.data))
+        .catch(err => console.log(err))
+    }, [])
+
+
+
     return (
         <div>
             hello world
