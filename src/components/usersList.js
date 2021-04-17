8f34b7b809dc2e5aaa15afb2dddb46297c26c64d
George Emad <george.emad@outlook.com>
2021-04-17 17:49:10 +0200

    NEW: sorting ascending & descending

--- src/components/usersList.js
+++ src/components/usersList.js
@@ -5,6 +5,7 @@ function Users() {
 
     const [users,setUsers] = useState([])
     const [interests,setInterests] = useState()
+    const [sortBoolean, setSortBoolean] = useState(false)
 
     useEffect(() => {
         // get users
@@ -22,6 +23,12 @@ function Users() {
                     count: data?.filter(c => c.id !== element.id && c.following.indexOf(element.id) > -1)?.length
                 })
             })
+            if(sortBoolean === 'ascending') {
+                newUsers.sort((a,b)=> a?.count - b?.count)
+            } else if (sortBoolean === 'descending') {
+                newUsers.sort((a,b)=> b?.count - a?.count)
+
+            }
             setUsers([...newUsers]) 
         })
         .catch(err => console.log(err))
@@ -31,17 +38,20 @@ function Users() {
         .then(res => setInterests(res.data)) 
         .catch(err => console.log(err))
         
-    }, [])
+    }, [sortBoolean])
 
    
-
-    
-
-
-
 console.log('users', 
 users
 )
+
+useEffect(() => {
+    // setUsers(users?.sort((a,b)=> a?.count - b?.count))
+}, [sortBoolean])
+
+const sort  = (sort) => {
+    setSortBoolean(sort)
+}
 
     return (
         <div>
@@ -55,6 +65,9 @@ users
                         </ul>
                     </div>
                 ))}
+                <button onClick={()=> sort('ascending')}>ascending sort</button>
+                <button onClick={()=> sort('descending')}>descending sort</button>
+
             </div>
         </div>
     )
