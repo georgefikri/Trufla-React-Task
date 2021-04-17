f189060b1af0775d9d0d995eca7cc584a37e0cff
George Emad <george.emad@outlook.com>
2021-04-17 17:26:27 +0200

    UPD: remove commented code

--- src/components/usersList.js
+++ src/components/usersList.js
@@ -1,14 +1,10 @@
 import React, {useState,useEffect} from 'react'
 import axios from 'axios'
 
-
-
 function Users() {
 
     const [users,setUsers] = useState([])
-    // const [usersWithCount, setUsersWithCount] = useState()
     const [interests,setInterests] = useState()
-    // const [aaa, setAaa] = useState(false)
 
     useEffect(() => {
         // get users
@@ -16,9 +12,7 @@ function Users() {
         .then(({data}) => {
             console.log('data', data)
 
-            // setUsers([...data.forEach((element)=> element.count = 
-            // data?.filter(c => c.id !== element.id && c.following.indexOf(element.id) > -1)?.length
-            // )])
+    
 
             let newUsers = []
 
@@ -39,34 +33,11 @@ function Users() {
         
     }, [])
 
-    // let followersCount = users?.map(user => {
-    //     const followersLength = users?.filter(c => c.id !== user.id && c.following.indexOf(user.id) > -1)?.length;
-    //     return { id: user.id, followers: followersLength };});
-
-    // useEffect(() => {
-    //     if(users) {
-    //         // console.log('0000', users)
-    //         // let newwww = users
-    //         // console.log('newwww', newwww)
-    //         // setUsers([...newwww, {count: 'hello'}])
-            // setUsers(newwww.forEach((element)=> element.count = 'hello'))
-            // setUsers(prevUsers => prevUsers.forEach((element)=> element.count = 
-            // prevUsers?.filter(c => c.id !== element.id && c.following.indexOf(element.id) > -1)?.length
-            // ))
-
-
-    //     }
-    // }, [users])
+   
 
     
 
-//  let followersCount = users?.map(user => {
-//     const followersLength = users?.filter(c => c.id !== user.id && c.following.indexOf(user.id) > -1)?.length;
-//     return { id: user.id, followers: followersLength };});
 
-// console.log('++++', 
-// followersCount
-// )
 
 console.log('users', 
 users
@@ -80,7 +51,7 @@ users
                     <div key={user?.name}>
                         <ul>
                             <li>name: {user?.name}</li>
-                            <li>count: {user?.count}</li>
+                            <li>followers count: {user?.count}</li>
                         </ul>
                     </div>
                 ))}
